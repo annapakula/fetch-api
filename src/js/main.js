202acffcd63212c280482e0517d78b25b63d2cec
Anna Pakuła <anna.pakula.321@gmail.com>
2020-09-13 13:10:18 +0200

    Change username in fetch path

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,11 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
 const list = document.querySelector('.list--js');
 
-fetch ("https://api.github.com/users/annamazurek/repos?sort=created&direction=desc")
+fetch ("https://api.github.com/users/annapakula/repos?sort=created&direction=desc")
   .then (resp => resp.json())
   .then (resp => {
-    const repositories = resp; //żeby było czytelniej (dobra praktyka)
+    const repositories = resp;
     for (const {name, html_url} of repositories){
       list.innerHTML += `<li class="list__item"><a class="list__link" href="${html_url}">${name}</a></li>`;
     
